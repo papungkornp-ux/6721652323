904fd72ce5043fc7c4d2ca840a4c2634b05aa1ab
papungkornp-ux <papungkorn.p@ku.th>
2025-12-29 16:50:55 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+/*let num1 = 5
+let num2 = 10
+let condition =!(num1 >= 3 || num2 >= 10);
+console.log("condition: ",condition);
+
+let age = 25;
+let gender = "ชาย";
+
+if(age >=20 && gender == "ชาย"){
+    console.log("ผู้ชายที่มีอายุ 20 ปีขึ้นไป");
+}*/
+/*let num = 20
+if(num % 2 ==0){
+    console.log("เป็นเลขคู่")
+}*/
+let counter = -5;
+while(counter < 10){
+    console.log("Hello world");
+    //counter=counter+1;
+    //counter +=1;
+    counter ++;
+}
+
+for(let i =0; i<10; i++){
+    console.log('Hello World from for loop');
+}
